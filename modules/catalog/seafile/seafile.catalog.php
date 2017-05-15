0ffbd25e65e668691007b49b7d4f0e1dc0b7daf8
Samuel Shores <Samuel Shores>
2017-05-15 01:28:23 -0500

    add seafile caalog clien

---
+++ modules/catalog/seafile/seafile.catalog.php
@@ -0,0 +1,514 @@
+<?php
+/* vim:set softtabstop=4 shiftwidth=4 expandtab: */
+/**
+ *
+ * LICENSE: GNU Affero General Public License, version 3 (AGPLv3)
+ * Copyright 2001 - 2016 Ampache.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * Seafile Catalog Class
+ *
+ * This class handles all actual work in regards to remote Seafile catalogs.
+ *
+ */
+
+use Seafile\Client\Resource\Directory;
+use Seafile\Client\Resource\File;
+use Seafile\Client\Resource\Library;
+use Seafile\Client\Http\Client;
+
+
+class Catalog_Seafile extends Catalog
+{
+    private $version        = '000001';
+    private $type           = 'seafile';
+    private $description    = 'Seafile Remote Catalog';
+
+    public $server_uri;
+    public $api_key;
+    public $library_name; // TODO
+
+    /**
+     * get_description
+     * This returns the description of this catalog
+     */
+    public function get_description()
+    {
+        return $this->description;
+    } // get_description
+
+    /**
+     * get_version
+     * This returns the current version
+     */
+    public function get_version()
+    {
+        return $this->version;
+    } // get_version
+
+    /**
+     * get_type
+     * This returns the current catalog type
+     */
+    public function get_type()
+    {
+        return $this->type;
+    } // get_type
+
+    /**
+     * get_create_help
+     * This returns hints on catalog creation
+     */
+    public function get_create_help()
+    {
+        $help = "<ul><li>" . T_("Install a Seafile server per its documentation (https://www.seafile.com/)") . "</li>" .
+            "<li>" . T_("") . "</li>" .
+            "<li>" . T_("If desired, create a user for Ampache and share with it the Library you want it to use. Otherwise, you can use your own credentials (not recommended).") . "</li>" .
+            "<li>" . T_("Enter username and password below.") . "</li>" .
+            "<li>&rArr;&nbsp;" . T_("After preparing the catalog with pressing the 'Add catalog' button,<br /> you have to 'Make it ready' on the catalog table.") . "</li></ul>";
+        return $help;
+    } // get_create_help
+
+    /**
+     * is_installed
+     * This returns true or false if remote catalog is installed
+     */
+    public function is_installed()
+    {
+        $sql        = "SHOW TABLES LIKE 'catalog_seafile'";
+        $db_results = Dba::query($sql);
+
+        return (Dba::num_rows($db_results) > 0);
+    } // is_installed
+
+    /**
+     * install
+     * This function installs the remote catalog
+     */
+    public function install()
+    {
+        $sql = "CREATE TABLE `" . 'catalog_' . $this->get_type() . "` (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY , " .
+            "`server_uri` VARCHAR( 255 ) COLLATE utf8_unicode_ci NOT NULL , " .
+            "`api_key` VARCHAR( 100 ) COLLATE utf8_unicode_ci NOT NULL , " .
+            "`library_name` VARCHAR( 255 ) COLLATE utf8_unicode_ci NOT NULL , " .
+            "`catalog_id` INT( 11 ) NOT NULL" .
+            ") ENGINE = MYISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci";
+        $db_results = Dba::query($sql);
+
+        return true;
+    }
+
+    public function catalog_fields()
+    {
+        $fields['server_uri'] = array('description' => T_('Server URI'), 'type'=>'text', 'value' => 'https://seafile.example.org/');
+        $fields['library_name'] = array('description' => T_('Library Name'), 'type'=>'text');
+
+        return $fields;
+    }
+
+    public function isReady()
+    {
+        return (!empty($this->api_key));
+    }
+
+    public function show_ready_process()
+    {
+        $this->requestCredentials();
+    }
+
+    protected function requestCredentials()
+    {
+        echo '<br />' . T_('Enter Seafile Username & Password') . '<br />';
+        echo "<form action='" . get_current_path() . "' method='post' enctype='multipart/form-data'>";
+        if ($_REQUEST['action']) {
+            echo "<input type='hidden' name='action' value='" . scrub_in($_REQUEST['action']) . "' />";
+            echo "<input type='hidden' name='catalogs[]' value='" . $this->id . "' />";
+        }
+        echo "<input type='hidden' name='perform_ready' value='true' />";
+
+        echo "Username: <input type='text' name='seafileusername' /> ";
+        echo "Password: <input type='password' name='seafilepassword' /> ";
+
+        echo "<input type='submit' value='" . T_("Connect to Seafile") . "' />";
+        echo "</form>";
+        echo "<br />";
+    }
+
+    public function perform_ready()
+    {
+        $password = $_REQUEST['seafilepassword'];
+        $username = $_REQUEST['seafileusername'];
+
+        $this->requestAuthToken($username, $password);
+    }
+
+    protected function requestAuthToken($username, $password)
+    {
+        $data = array('username' => $username, 'password' => $password);
+
+        // use key 'http' even if you send the request to https://...
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($this->server_uri, false, $context);
+
+        if ($result === FALSE) {
+            AmpError::add('general', T_('Error: Could not authenticate against Seafile API.'));
+        }
+
+        $token = json_decode($result);
+
+        $this->api_key = $token;
+
+        debug_event('seafile_catalog', 'Retrieved API token for user ' . $userId . '.', 1);
+
+        $sql = 'UPDATE `' . 'catalog_' . $this->get_type() . '` SET `api_key` = ? WHERE `catalog_id` = ?';
+        Dba::write($sql, array($this->authtoken, $this->catalog_id));
+    }
+
+    /**
+     * create_type
+     *
+     * This creates a new catalog type entry for a catalog
+     */
+    public static function create_type($catalog_id, $data)
+    {
+        $server_uri = trim($data['server_uri']);
+        $api_key    = trim($data['api_key']);
+        $library_name = trim($data['library_name']);
+
+        if (!strlen($server_uri)) {
+            AmpError::add('general', T_('Error: Seafile Server URL is required.'));
+            return false;
+        }
+
+        if (!strlen($api_key)) {
+            AmpError::add('general', T_('Error: Could not verify Seafile Server credentials.'));
+            return false;
+        }
+
+        if (!strlen($library_name)) {
+            AmpError::add('general', T_('Error: Seafile Server Library Name is required.'));
+            return false;
+        }
+
+        $sql = 'INSERT INTO `' . 'catalog_' . $this->get_type() . '` (`server_uri`, `api_key`, `library_name`, `catalog_id`) VALUES (?, ?, ?, ?)';
+        Dba::write($sql, array($server_uri, $api_key, $library_name, $catalog_id));
+        return true;
+    }
+
+    /**
+     * Constructor
+     *
+     * Catalog class constructor, pulls catalog information
+     */
+    public function __construct($catalog_id = null)
+    {
+        if ($catalog_id) {
+            $this->id = intval($catalog_id);
+            $info     = $this->get_info($catalog_id);
+
+            foreach ($info as $key=>$value) {
+                $this->$key = $value;
+            }
+        }
+    }
+
+    public function createClient()
+    {
+        $client = new Client([
+            'base_uri' => $this->server_uri,
+            'debug' => false,
+            'headers' => [
+                'Authorization' => 'Token ' . $this->api_key
+            ]
+        ]);
+
+        return array(
+            'Libraries' => new Library($client),
+            'Directories' => new Directory($client),
+            'Files' => new File($client)
+        );
+    }
+
+    /**
+     * add_to_catalog
+     * this function adds new files to an
+     * existing catalog
+     */
+    public function add_to_catalog($options = null)
+    {
+        // Prevent the script from timing out
+        set_time_limit(0);
+
+        if (!defined('SSE_OUTPUT')) {
+            UI::show_box_top(T_('Running Seafile Remote Update') . '. . .');
+        }
+        $this->add_from_library();
+
+        if (!defined('SSE_OUTPUT')) {
+            UI::show_box_bottom();
+        }
+
+        return true;
+    }
+
+    /**
+     * add_from_library
+     *
+     * Pulls the data from a remote catalog and adds any missing songs to the
+     * database.
+     */
+    private function add_from_library()
+    {
+        $client = $this->createClient();
+
+        $libraries = $client['Libraries']->getAll();
+
+        $library = array_filter($libraries, function($lib) { return $lib->name == $library_name; });
+
+        if(count($library) == 0) {
+            AmpError::add('general', sprintf(T_('No media updated: could not find Seafile library called "%s"')), $library_name);
+        }
+
+        $count = $this->add_from_directory($client, $library[0], '/');
+
+        UI::update_text('', sprintf(T_('Catalog Update Finished.  Total Media: [%s]'), $count));
+
+        if ($count == 0) {
+            AmpError::add('general', T_('No media updated, do you respect the patterns?'));
+        }
+
+        return true;
+    }
+
+    /**
+     * add_from_directory
+     *
+     * Recurses through directories and pulls out all media files
+     */
+    public function add_from_directory($client, $library, $path)
+    {
+        $directoryItems = $client['Directories']->getAll($library, $path);
+
+        $count = 0;
+
+        if ($directoryItems !== null && count($directoryItems) > 0) {
+            foreach ($directoryItems as $item) {
+                if ($item->type == 'dir') {
+                    $count += $this->add_from_directory($client, $library, $path . $item->name . '/');
+                } else if ($item->type == 'file') {
+                    $count += $this->add_file($client, $item);
+                }
+            }
+        }
+
+        return $count;
+    }
+
+    public function add_file($client, $file)
+    {
+        $filesize = $file->size;
+
+        if ($file->size > 0) {
+            $is_audio_file = Catalog::is_audio_file($file->name);
+            $is_video_file = Catalog::is_video_file($file->name);
+
+            if(!$is_audio_file && !$is_video_file) {
+                debug_event('read', $data['path'] . " ignored, unknown media file type", 5);
+            }
+
+            if ($is_audio_file && count($this->get_gather_types('music')) > 0) {
+                $this->insert_song($client, $file);
+                return 1;
+            } else if ($is_video_file && count($this->get_gather_types('video')) > 0) {
+                // TODO $this->insert_video()
+                return 1;
+            } else {
+                debug_event('read', $data['path'] . " ignored, bad media type for this catalog.", 5);
+            }
+        } else {
+            debug_event('read', $data['path'] . " ignored, 0 bytes", 5);
+        }
+
+        return 0;
+    }
+
+    /**
+     * _insert_local_song
+     *
+     * Insert a song that isn't already in the database.
+     */
+    private function insert_song($client, $file)
+    {
+        if ($this->check_remote_song($this->get_virtual_path($file))) {
+            debug_event('seafile_catalog', 'Skipping existing song ' . $file, 5);
+        } else {
+            $origin  = $file;
+            $islocal = false;
+            $fpchunk = 0;
+            // Get temporary chunked file from Dropbox to (hope) read metadata
+            if ($this->getchunk) {
+                $fpchunk  = tmpfile();
+                $metadata = $client->getFile($file, $fpchunk, null, 40960);
+                if ($metadata == null) {
+                    debug_event('dropbox_catalog', 'Cannot get Dropbox file: ' . $file, 5);
+                }
+                $streammeta = stream_get_meta_data($fpchunk);
+                $file       = $streammeta['uri'];
+                $islocal    = true;
+            }
+
+            $vainfo = new vainfo($file, $this->get_gather_types('music'), '', '', '', $this->sort_pattern, $this->rename_pattern, $islocal);
+            $vainfo->forceSize($filesize);
+            $vainfo->get_info();
+
+            $key     = vainfo::get_tag_type($vainfo->tags);
+            $results = vainfo::clean_tag_info($vainfo->tags, $key, $file);
+
+            // Remove temp file
+            if ($fpchunk) {
+                fclose($fpchunk);
+            }
+
+            // Set the remote path
+            $results['file']    = $origin;
+            $results['catalog'] = $this->id;
+
+            if (!empty($results['artist']) && !empty($results['album'])) {
+                $results['file'] = $this->get_virtual_path($results['file']);
+
+                $this->count++;
+                return Song::insert($results);
+            } else {
+                debug_event('results', $results['file'] . " ignored because it is an orphan songs. Please check your catalog patterns.", 5);
+            }
+        }
+
+        return false;
+    }
+
+    public function verify_catalog_proc()
+    {
+        return array('total' => 0, 'updated' => 0);
+    }
+
+    /**
+     * clean_catalog_proc
+     *
+     * Removes songs that no longer exist.
+     */
+    public function clean_catalog_proc()
+    {
+        $dead = 0;
+
+        $client = $this->createClient();
+        if ($client != null) {
+            $sql        = 'SELECT `id`, `file` FROM `song` WHERE `catalog` = ?';
+            $db_results = Dba::read($sql, array($this->id));
+            while ($row = Dba::fetch_assoc($db_results)) {
+                debug_event('dropbox-clean', 'Starting work on ' . $row['file'] . '(' . $row['id'] . ')', 5, 'ampache-catalog');
+                $file     = $this->get_rel_path($row['file']);
+                $metadata = $client->getMetadata($file);
+                if ($metadata) {
+                    debug_event('dropbox-clean', 'keeping song', 5, 'ampache-catalog');
+                } else {
+                    debug_event('dropbox-clean', 'removing song', 5, 'ampache-catalog');
+                    $dead++;
+                    Dba::write('DELETE FROM `song` WHERE `id` = ?', array($row['id']));
+                }
+            }
+        } else {
+            AmpError::add('general', T_('API Error: cannot connect to Dropbox.'));
+        }
+
+        return $dead;
+    }
+
+    /**
+     * check_remote_song
+     *
+     * checks to see if a remote song exists in the database or not
+     * if it find a song it returns the UID
+     */
+    public function check_remote_song($file)
+    {
+        $sql        = 'SELECT `id` FROM `song` WHERE `file` = ?';
+        $db_results = Dba::read($sql, array($file));
+
+        if ($results = Dba::fetch_assoc($db_results)) {
+            return $results['id'];
+        }
+
+        return false;
+    }
+
+    public function get_virtual_path($file)
+    {
+        return $this->apikey . '|' . $file;
+    }
+
+    public function get_rel_path($file_path)
+    {
+        $p = strpos($file_path, "|");
+        if ($p !== false) {
+            $p++;
+        }
+        return substr($file_path, $p);
+    }
+
+    /**
+     * format
+     *
+     * This makes the object human-readable.
+     */
+    public function format()
+    {
+        parent::format();
+        $this->f_info      = $this->api_key;
+        $this->f_full_info = $this->api_key;
+    }
+
+    public function prepare_media($media)
+    {
+        $client = $this->createClient();
+        if ($client != null) {
+            set_time_limit(0);
+
+            // Generate browser class for sending headers
+            $browser    = new Horde_Browser();
+            $media_name = $media->f_artist_full . " - " . $media->title . "." . $media->type;
+            $browser->downloadHeaders($media_name, $media->mime, false, $media->size);
+            $file = $this->get_rel_path($media->file);
+
+            $output   = fopen('php://output', 'w');
+            $metadata = $client->getFile($file, $output);
+            if ($metadata == null) {
+                debug_event('play', 'File not found on Dropbox: ' . $file, 5);
+            }
+            fclose($output);
+        }
+
+        return null;
+    }
+}
